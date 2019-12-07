0b5878abd8ea686bb848d62d94348aff208563fe
jiaxionghu <374945747@qq.com>
2019-12-07 11:19:32 +0800

    Minitest

--- Minitest/src/Mapping.java
+++ Minitest/src/Mapping.java
@@ -4,30 +4,30 @@ import java.util.List;
 
 public class Mapping {
 
-    public List<String> resolve(int [] digits) {
-        LinkedList<String> ans = new LinkedList<String>();
+    public static List<String> resolve(int [] digits) {
+        LinkedList<String> linkedList = new LinkedList<String>();
         if (digits == null || digits.length == 0) {
-            return ans;
+            return linkedList;
         }
-        String[] intToStringArray = new String[]{"0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        String[] mappingArray = new String[]{"0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
         int x, n = digits.length;
         String s;
-        ans.add("");
+        linkedList.add("");
         for (int i = 0; i < n; i++) {
-            while (ans.peek().length() == i) {
-                s = ans.remove();
+            while (linkedList.peek().length() == i) {
+                s = linkedList.remove();
                 x = digits[i];
-                for (char c : intToStringArray[x].toCharArray()) {
-                    ans.add(s + c);
+                for (char c : mappingArray[x].toCharArray()) {
+                    linkedList.add(s + c);
                 }
             }
         }
-        return ans;
+        return linkedList;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Mapping().resolve(new int[]{0,2}));//[0A, 0B, 0C]
-        System.out.println(new Mapping().resolve(new int[]{2,3,4}));//[ADG, ADH, ADI, AEG, AEH, AEI, AFG, AFH, AFI, BDG, BDH, BDI, BEG, BEH, BEI, BFG, BFH, BFI, CDG, CDH, CDI, CEG, CEH, CEI, CFG, CFH, CFI]
+        System.out.println(resolve(new int[]{0,2}));//[0A, 0B, 0C]
+        System.out.println(resolve(new int[]{2,3,4}));//[ADG, ADH, ADI, AEG, AEH, AEI, AFG, AFH, AFI, BDG, BDH, BDI, BEG, BEH, BEI, BFG, BFH, BFI, CDG, CDH, CDI, CEG, CEH, CEI, CFG, CFH, CFI]
     }
 }
 
